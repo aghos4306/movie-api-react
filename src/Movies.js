305bfb9f5be25935e8eec422bf23fd6deb651a87
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2021-05-09 02:03:51 +0300

    render movies on Movies component

--- src/Movies.js
+++ src/Movies.js
@@ -5,7 +5,33 @@ const url =
   'https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png'
 
 const Movies = () => {
-  return <h2>movies component</h2>
+  const { movies, isLoading } = useGlobalContext()
+
+  if(isLoading) {
+    return (
+      <div className="loading"></div>
+    )
+  }
+  return (
+    <section className="movies">
+      {movies.map((movie) => {
+        const { Title: title, Year: year, imdbID: id, Type: type, Poster: poster } = movie
+        console.log(movie)
+        return (
+          // <h4 key={id}>Movies</h4>
+          <Link to={`movies/${id}`} key={id} className="movie">
+            <article>
+              <img src={poster === 'N/A' ? url : poster} alt={title} />
+              <div className="movie-info">
+                <h4 className="title">{title}</h4>
+                <p>{year}</p>
+              </div>
+            </article>
+          </Link>
+        )
+      })}
+    </section>
+  )
 }
 
 export default Movies
